a3eed2f14d22a89fd52b0686f63f4ce366cad819
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-04-15 09:32:10 +0200

    feat: add fake api like jonas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
-import DateCounter from "./DateCounter";
+import DateCounter from "./components/DateCounter";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import "./index.css";
+import { useEffect } from "react";
+import { useReducer } from "react";
+function reducer2(state, action) {
+  switch (action.type) {
+    case "setQuestions":
+      return action.payload;
+    default:
+      throw new Error("unknow Error");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer2, []);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "setQuestions", payload: data }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main />
     </div>
   );
 }
