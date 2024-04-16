5bca9975e5022d331e135b61cd91da23113cb370
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-04-16 07:38:11 +0200

    feat: manage state
    - add initialState object
    - implement reducer function

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,63 @@ import Main from "./components/Main";
 import "./index.css";
 import { useEffect } from "react";
 import { useReducer } from "react";
-function reducer2(state, action) {
-  switch (action.type) {
+const sec_per_question = 10;
+const initialState = {
+  questions: [],
+  index: 0, // for question number
+  points: 0,
+  answer: null,
+  // loading, error, ready, active, finished
+  status: "loading",
+  maxScore: 0,
+  timer: 10,
+};
+function reducer(state, action) {
+  const { type, payload } = action;
+  const { index, questions, points, maxScore, timer } = state;
+  switch (type) {
     case "setQuestions":
-      return action.payload;
+      return { ...state, questions: payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        timer: sec_per_question * questions.length,
+      };
+    case "next":
+      return {
+        ...state,
+        index: index + 1,
+        answer: null,
+        status: index >= questions.length - 1 ? "finished" : "active",
+        maxScore: Math.max(points, maxScore),
+      };
+    case "inc":
+      return {
+        ...state,
+        answer: payload,
+        points:
+          points +
+          (payload === questions[index].correctOption
+            ? questions[index].points
+            : 0),
+      };
+    case "restart":
+      return {
+        ...initialState,
+        status: "ready",
+        questions: questions,
+        maxScore: maxScore,
+      };
+    case "tick":
+      return {
+        ...state,
+        timer: timer - 1,
+        status: timer === 0 ? "finished" : state.status,
+      };
+
     default:
       throw new Error("unknow Error");
   }
